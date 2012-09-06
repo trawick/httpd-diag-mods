c848a1d795ce0360a0a1740712515f7bc7482fa5
Jeff Trawick <trawick@gmail.com>
2012-09-06 11:57:28 -0400

    incomplete

---
+++ makezip.py
@@ -0,0 +1,10 @@
+
+
+source_files = ['Makefile', 'makefile.win32', '*.c', '*.h']
+meta_files = ['CHANGES.txt', 'LICENSE.txt', 'NOTICE.txt', 'README.txt']
+bin_files = ['dist']
+
+version = '2.00'
+
+dirname = 'wku_bt-%s' % (version)
+
